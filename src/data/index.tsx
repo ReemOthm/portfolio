18be2918c89894709b5d10ec86b727b2e37d9676
ReemOthm <ReemOth@outlook.com>
2024-12-04 13:46:56 +0300

    edited 4

--- src/data/index.tsx
+++ src/data/index.tsx
@@ -8,7 +8,7 @@ export const projectsData: {
 }[] = [
     {
         "projectName": "ShopStore",
-        "projectImage": "../../public/images/shopstore.png",
+        "projectImage": "../images/shopstore.png",
         "deployLink": "https://incandescent-churros-6970f1.netlify.app/",
         "githubRrpo": "https://github.com/ReemOthm/sda-html-css-ecommerce-landing-page",
         "description": "ShopStore is an e-commerce landing page devloped using HTML, CSS, Animations, and the Web Accessibility.",
@@ -16,7 +16,7 @@ export const projectsData: {
     },
     {
         "projectName": "Todo App",
-        "projectImage": "../../public/images/todo-app.png",
+        "projectImage": "../images/todo-app.png",
         "deployLink": "https://tubular-conkies-452f5d.netlify.app/",
         "githubRrpo": "https://github.com/ReemOthm/sda-javascript-todo-app",
         "description": "This project is a to do list application by applying HTML, CSS and Javascript. The application allows users to manage their tasks effectively.",
@@ -24,7 +24,7 @@ export const projectsData: {
     },
     {
         "projectName": "React Budget App",
-        "projectImage": "../../public/images/react-budget-app.png",
+        "projectImage": "../images/react-budget-app.png",
         "deployLink": "https://tranquil-paprenjak-6cd5ee.netlify.app/",
         "githubRrpo": "https://github.com/ReemOthm/sda-react-budget-app",
         "description": "This repository contains a React application to manage incomes, expenses, and saving targets. The application is built using TypeScript and focuses on fundamental concepts of React.",
@@ -32,7 +32,7 @@ export const projectsData: {
     },
     {
         "projectName": "HpmeDecor",
-        "projectImage": "../../public/images/homedecor.png",
+        "projectImage": "../images/homedecor.png",
         "deployLink": "https://main--homedecore-project.netlify.app/",
         "githubRrpo": "https://github.com/ReemOthm/home-decor-frontend",
         "description": "Home-Decor is an ecommerce website that provide a collection of home decoration and accessories products.",
